95ac4eceb85941ed66ef3ab36c6273902175d113
Andreas Haja <ah@andreashaja.com>
2020-12-12 07:33:18 +0000

    conversion complete

--- 2d_to_1d.cpp
+++ 2d_to_1d.cpp
@@ -7,8 +7,10 @@ int main ()
 {
   // Zeilen und Spalten zählen
   int num_rows{0}, num_cols{0};
-  string text_1d, line;
-  fstream input_stream("ascii-art-0-1d.txt", fstream::in);
+  string text_1d, line, filename_in, filename_out;
+  filename_in = "ascii-art-0-2d.txt";
+  filename_out = "ascii-art-0-1d.txt";
+  fstream input_stream(filename_in, fstream::in);
   while( getline ( input_stream, line ) )
   {
     ++num_rows;
@@ -18,4 +20,11 @@ int main ()
   cout << "num_rows = " << num_rows << ", num_cols = " << num_cols << ", text_1d = " << text_1d << endl;
 
   // Ergebnis im 1D-Format speichern
+  fstream output_stream(filename_out, fstream::out);
+  output_stream << num_rows << endl << num_cols << endl << text_1d;
+
+  // Dateie schließen
+  input_stream.close();
+  output_stream.close();
+
 }
